95b403a6109c2c0331d066bfd948e7443fae6d3e
Sam A. Welch (NIVA) <saw@niva.no>
2026-03-31 12:23:51 +0200

    Attempting a link to eDataDRF in mod_compartments_fct.R

--- R/mod_compartments_fct_.R
+++ R/mod_compartments_fct_.R
@@ -1,21 +1,23 @@
 #' Create new compartment combination row
 #'
 #' Creates a single-row tibble with specified compartment information,
-#' using the standardised structure from initialise_compartments_tibble().
+#' using the standardised structure from [eDataDRF::initialise_campaign_tibble()].
+#' Function will return an error if compartment, sub_compartment, or category are
+#' not of the correct variable type, but it doesn't check for vocabulary
 #'
-#' @param compartment Character string specifying the environmental compartment
-#' @param sub_compartment Character string specifying the sub-compartment
-#' @param category Character string specifying the measured category
+#' @param compartment Character string specifying the environmental compartment ([eDataDRF::environ_compartments_vocabulary()])
+#' @param sub_compartment Character string specifying the sub-compartment ([eDataDRF::environ_compartments_sub_vocabulary()])
+#' @param category Character string specifying the measured category ([eDataDRF::measured_category()])
 #'
 #' @return tibble with one row containing the specified compartment information
 #' @family create
 #' @importFrom dplyr add_row
 #' @import eDataDRF
 #' @examples
-#' create_compartment_combination("Water", "Surface water", "Abiotic")
-#' create_compartment_combination("Biota", "Fish", "Biotic")
+#' create_compartment_combination("Aquatic", "Surface water", "External")
+#' create_compartment_combination("Biota", "Biota, Aquatic", "Internal")
 #' @export
-#' @seealso \code{\link{initialise_compartments_tibble}}
+#' @seealso [eDataDRF::initialise_campaign_tibble()]
 create_compartment_combination <- function(
   compartment,
   sub_compartment,
